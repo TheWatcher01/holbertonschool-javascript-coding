5931e7687b1533ad8f8a9d11e54f3e6d10eb5de2
TheWatcher01 <7450@holbertonstudents.com>
2024-05-14 15:44:48 +0200

    Add 6-completed_tasks.js for task 6. How many completed?

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,53 @@
+#!/usr/bin/node
+
+/**
+ * @file 6-completed_tasks.js
+ * @author TheWatcher01
+ * @date 14-05-2024
+ * @description This script computes the number of tasks completed by each user ID.
+ */
+
+'use strict';
+// The 'use strict' directive is used to enforce stricter parsing and error handling on your JavaScript code at runtime.
+
+const request = require('request');
+// The 'request' module is imported. This module allows you to make HTTP requests in a simple way.
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+// The URL for the GET request is defined. This URL points to a JSON placeholder API that returns a list of todos.
+
+request(url, function (error, response, body) {
+  // The 'request' function from the 'request' module is used to make a GET request to the specified URL.
+  // The callback function takes three arguments: 'error', 'response', and 'body'.
+
+  if (error) {
+    // If there's an error with the request (like a network problem), it will be logged to the console and the function will return early.
+    console.error(error);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  // The body of the response is parsed from JSON into a JavaScript object and stored in the 'todos' variable.
+
+  const completedTasks = {};
+  // An empty object is created to store the number of completed tasks for each user.
+
+  todos.forEach(todo => {
+    // The 'forEach' method is used to execute a function on each item in the 'todos' array.
+
+    if (todo.completed) {
+      // If the 'completed' property of the todo is true, the count of completed tasks for the user is incremented.
+
+      if (completedTasks[todo.userId]) {
+        // If the user already has a count in the 'completedTasks' object, their count is incremented.
+        completedTasks[todo.userId]++;
+      } else {
+        // If the user does not have a count in the 'completedTasks' object, their count is set to 1.
+        completedTasks[todo.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasks);
+  // The 'completedTasks' object is logged to the console, it contains number of completed tasks for each user.
+});
