bdb5111dd510cd986bfbb748ef12edcd6e878b0b
TheWatcher01 <7450@holbertonstudents.com>
2024-05-14 12:33:29 +0200

    Add 3-starwars_title.js for task 3. Star wars movie title

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,42 @@
+#!/usr/bin/node
+
+/**
+ * @file 3-starwars_title.js
+ * @author TheWatcher01
+ * @date 14-05-2024
+ * @description This script prints title of a Star Wars movie where episode number matches a given integer.
+ */
+
+'use strict';
+// The 'use strict' directive switches script to strict mode, which helps catch common coding mistakes & "unsafe" actions.
+
+const request = require('request');
+
+const movieId = process.argv[2];
+// The movie ID for the GET request is taken from the command line arguments.
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+// The URL for the GET request is constructed using the movie ID.
+
+request(url, function (error, response, body) {
+  // The 'request' function of the 'request' module is used to make a GET request to the specified URL.
+  // The callback function takes three arguments: 'error', 'response', and 'body'.
+
+  if (error) {
+    // If there is an error with the request (like a network problem), it will be logged to the console and the function will return early.
+    console.error('error:', error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  // The 'body' of the response is parsed from JSON to a JavaScript object.
+
+  if (movie.detail === 'Not found') {
+    // If the movie detail is 'Not found', a message is logged to the console and the function returns early.
+    console.log('No movie found with this ID');
+    return;
+  }
+
+  console.log(movie.title);
+  // If there is no error and the movie is found, the title of the movie is logged to the console.
+});
