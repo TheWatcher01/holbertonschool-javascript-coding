7a822d4493118c942980762c8e96640bb9ac1752
TheWatcher01 <7450@holbertonstudents.com>
2024-05-27 13:40:55 +0200

    Add countStudents function to 3-read_file_async.js

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -3,5 +3,33 @@
  * @file 3-read_file_async.js
  * @author TheWatcher01
  * @date 27-05-2024
- * @description 
+ * @description
  */
+
+const fs = require('fs').promises;
+
+async function countStudents(pathFile) {
+  try {
+    const data = await fs.readFile(pathFile, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1).map((line) => line.split(','));
+
+    const totalStudents = students.length;
+    console.log(`Number of students: ${totalStudents}`);
+
+    const studentsByFields = {};
+    students.forEach((student) => {
+      const field = student[3];
+      if (!studentsByFields[field]) studentsByFields[field] = [];
+      studentsByFields[field].push(student[0]);
+    });
+
+    for (const [field, names] of Object.entries(studentsByFields)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
