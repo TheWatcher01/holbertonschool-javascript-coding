99bfaf27d4003b8615a286fa5f07d7eda85490ad
TheWatcher01 <7450@holbertonstudents.com>
2024-05-27 13:56:13 +0200

    Update Node_JS_basic/4-http.js to include a small HTTP server using Node's HTTP module

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,5 +3,21 @@
  * @file 4-http.js
  * @author TheWatcher01
  * @date 27-05-2024
- * @description 
+ * @description Module contains a small HTTP server using Node's HTTP module.
  */
+
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+const port = 1245;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = app;
