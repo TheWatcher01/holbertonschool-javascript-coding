ae403fa5ac0fbbddbc655c6de658bbd1c7f301d5
TheWatcher01 <7450@holbertonstudents.com>
2024-05-27 14:19:03 +0200

    Update Node_JS_basic/5-http.js to create a simple HTTP server using the http module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -3,5 +3,66 @@
  * @file 5-http.js
  * @author TheWatcher01
  * @date 27-05-2024
- * @description 
+ * @description Module that creates a simple HTTP server using the http module.
  */
+
+const http = require('http');
+const fs = require('fs').promises;
+const url = require('url');
+
+const countStudents = async (filePath) => {
+  try {
+    const data = await fs.readFile(filePath, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1).map((line) => line.split(','));
+
+    const totalStudents = students.length;
+    let response = `Number of students: ${totalStudents}\n`;
+
+    const studentsByFields = {};
+    students.forEach((student) => {
+      const field = student[3];
+      if (!studentsByFields[field]) studentsByFields[field] = [];
+      studentsByFields[field].push(student[0]);
+    });
+
+    for (const [field, names] of Object.entries(studentsByFields)) {
+      response += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+    }
+
+    return response.trim();
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+const app = http.createServer(async (req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  if (parsedUrl.pathname === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (parsedUrl.pathname === '/students') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    try {
+      const studentData = await countStudents(process.argv[2]);
+      res.end(`This is the list of our students\n${studentData}`);
+    } catch (error) {
+      res.statusCode = 500;
+      res.end(error.message);
+    }
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
+});
+
+const port = 1245;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = app;
