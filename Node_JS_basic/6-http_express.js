97cc6dd98dcc95f8d5fc92f0d6c16b495ab565d3
TheWatcher01 <7450@holbertonstudents.com>
2024-05-27 16:00:48 +0200

    Add 6-http_express.js to use Express for a simple HTTP server

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -3,5 +3,22 @@
  * @file 6-http_express.js
  * @author TheWatcher01
  * @date 27-05-2024
- * @description 
+ * @description Module contains small HTTP server using Express.
  */
+
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+// Define the root route
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = app;
