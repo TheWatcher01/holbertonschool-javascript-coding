da3e0b149dd7f6ad434dab5a3458136166d1b62f
TheWatcher01 <7450@holbertonstudents.com>
2024-05-27 11:53:09 +0200

    Add displayMessage function to 0-console.js

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -3,5 +3,11 @@
  * @file 0-console.js
  * @author TheWatcher01
  * @date 27-05-2024
- * @description 
+ * @description Module contains function named displayMessage that prints in STDOUT string argument.
  */
+
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
